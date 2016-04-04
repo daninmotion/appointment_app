61dc514563020d7e697f273f3ebde78920df2c20
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-04-04 14:51:06 -0400

    Config

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160404033633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string   "name"
     t.datetime "start_time"
